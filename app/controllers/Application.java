bf1ad4b025e18a1f4b9a6b06a598a75beebe9941
Daniel Chesters <daniel.chesters@gmail.com>
2010-05-30 04:11:21 +0200

    list (in homepage) and show application controller

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,21 @@
 package controllers;
 
+import java.util.List;
+
+import models.Topic;
+
 import play.mvc.*;
 
 public class Application extends Controller {
 
     public static void index() {
-        render();
+        List<Topic> topics = Topic.find("order by modified desc").fetch();
+        render(topics);
+    }
+
+    public static void show(Long id) {
+        Topic topic = Topic.findById(id);
+        render(topic);
     }
 
 }
