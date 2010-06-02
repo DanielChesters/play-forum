4c2fe0ad5a8cf7083dcad1ebe3b30214d458c267
Daniel Chesters <daniel.chesters@gmail.com>
2010-06-02 23:37:31 +0200

    add a create topic view and controller method

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -18,11 +18,24 @@ public class Application extends Controller {
         render(topic);
     }
 
-    public static void reply(Long topicId,String author, String subtitle, String text){
+    public static void add() {
+        render();
+    }
+
+    public static void reply(Long topicId, String author, String subtitle,
+            String text) {
         Topic topic = Topic.findById(topicId);
         topic.addPost(subtitle, text, author);
         flash.success("Your reply was posted");
         show(topicId);
     }
 
+    public static void createTopic(String title, String author,
+            String subtitle, String text) {
+        Topic topic = new Topic(title);
+        topic.addPost(subtitle, text, author);
+        topic.save();
+        flash.success("Your topic was created");
+        show(topic.id);
+    }
 }
