b6c73fbc64863586004ca91627146d829d23ef9d
Daniel Chesters <daniel.chesters@gmail.com>
2010-06-02 22:44:38 +0200

    add some unit tests

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -5,9 +5,49 @@ import models.*;
 
 public class BasicTest extends UnitTest {
 
-    @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
+    private Long topicId;
+
+    @Before
+    public void setup(){
+        Fixtures.deleteAll();
+        Topic topic = new Topic("title").save();
+        topicId = topic.id;
     }
+
+    @Test
+    public void createdTopic(){
+        Topic topic = Topic.findById(topicId);
+
+        assertNotNull(topic);
+        assertNotNull(topic.id);
+        assertEquals("title", topic.title);
+        assertNotNull(topic.created);
+        assertNull(topic.modified);
+        assertNotNull(topic.posts);
+        assertEquals(0, topic.posts.size());
+
+    }
+
+    @Test
+    public void createdPost() {
+        Topic topic = Topic.findById(topicId);
+
+        topic.addPost("subtitle", "text", "author");
+
+        assertNotNull(topic.modified);
+        assertEquals(1 , topic.posts.size());
+        Post post = topic.posts.get(0);
+
+        assertNotNull(post);
+        assertNotNull(post.id);
+        assertEquals("subtitle", post.subtitle);
+        assertEquals("text", post.text);
+        assertEquals("author", post.author);
+        assertNotNull(post.created);
+        assertNull(post.modified);
+        assertNotNull(post.topic);
+        assertEquals(topic, post.topic);
+    }
+
 
 }
